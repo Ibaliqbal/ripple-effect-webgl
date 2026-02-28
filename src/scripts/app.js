802fb8d622ed082c16a13a12bfd85ef102a0d846
Ibaliqbal <iqbalmuthahhary@gmail.com>
2026-02-28 20:49:14 +0700

    add preload image

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,9 @@
 import "../styles/app.css";
 import RippleCanvas from "./module/ripple-canvas";
+import { preloadImages } from "./utils";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  await preloadImages(".item img");
+
   new RippleCanvas();
 });
